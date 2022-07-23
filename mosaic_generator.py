6685b003329e5aeb77f13a1d4632d3b58c257cbb
John <salvadorjmassa@gmail.com>
2022-07-23 16:32:11 -0400

    Reworked mosaic script to operated independently from original version (see Capstone). Renamed recursive_file_save() to recursive_save(), and completed function. Renamed verify_CLA() to verify_args(), and reworked it for stand-alone version. Removed disk_usage(), perhaps to be added back at a later date. Removed create_dict_resize_save_tiles(), consolidated under create_tiles() Added pathname parameter to recursive_save() Created rename_mosaic() to create the mosaic filename. Added print statement recursive_save(). Added open_file() to open the file in the OS.

--- mosaic_generator.py
+++ mosaic_generator.py
@@ -23,18 +23,21 @@ from PIL import Image
 tile_percentage_num = .015
 
 
-def create_dict_resize_save_tiles(size, pathname):
+def create_tiles(template_image, size, image_dir):
     """
-    Resizes tiles, gets the rgb and saves to dict.
-    Saves file to new directory.
+    Creates RGB dictionary, saves to json, resizes and saves as tiles.
     """
 
+    # Creates a directory to hold tiles if it does not exist
+    tile_dir = image_dir+"_tiles"
+    if not os.path.exists(tile_dir):
+        os.mkdir(tile_dir)
 
-
-    # Resizes tiles and saves to a directory
+    # Creates RGB dictionary from tiles, saves to json file
+    # Resizes and saves tiles to a new directory
     tile_dict = {}
-    for file in os.listdir(pathname):
-        filename = os.path.join(pathname, file)
+    for file in os.listdir(image_dir):
+        filename = os.path.join(image_dir, file)
         with Image.open(filename).convert("RGB") as im:
             im1 = im.resize(size)
             # Get image average RGB while file is open, add to dictionary
@@ -44,41 +47,9 @@ def create_dict_resize_save_tiles(size, pathname):
             im1.save(os.path.join(tile_dir, file), "JPEG")
 
     # Save tile_dict to json file
-    json_filename = os.path.basename(pathname).split(".")[0]+"_tiles.json"
+    json_filename = os.path.basename(image_dir).split(".")[0]+"_tiles.json"
     with open(json_filename, "w") as outfile:
         json.dump(tile_dict, outfile)
-
-    return tile_dir, json_filename
-
-
-def recursive_file_save(file_name, pathname=""):
-    """
-
-    """
-
-    x = 0
-    if not os.path.exists(file_name):
-        # save file
-    else:
-        x += 1
-        file_name = file_name+f" ({x})"
-        recursive_file_save(file_name)
-
-
-
-def create_tiles(template_image, pathname):
-    """
-    Creates RGB dictionary, saves to json, resizes and saves as tiles.
-    """
-
-    # Creates a directory to hold tiles if it does not exist
-    tile_dir = pathname+"_tiles"
-    if not os.path.exists(tile_dir):
-        os.mkdir(tile_dir)
-
-    # Creates RGB dictionary from tiles, saves to json file
-    # Resizes and save tiles to new directory
-    tile_dir, json_filename = create_dict_resize_save_tiles(size, pathname)
 
     return tile_dir, json_filename
 
@@ -97,6 +68,7 @@ def get_rgb(img_object):
         g += pixel[1]
         b += pixel[2]
     average_rgb = [int(r/pixels), int(g/pixels), int(b/pixels)]
+
     return average_rgb
 
 def crop_mosaic_get_rgb(size, column, row, image):
@@ -118,7 +90,7 @@ def crop_mosaic_get_rgb(size, column, row, image):
 
     return portrait_rgb, coordinates
 
-def find_best_match(tile_dir, tile_data, portrait_rgb, im, coordinates):
+def find_best_match(tile_data, portrait_rgb):
     """
     Searches tile json data for closes rgb match to mosaic tile.
     """
@@ -137,12 +109,53 @@ def find_best_match(tile_dir, tile_data, portrait_rgb, im, coordinates):
             best_match_diff = diff
             best_match = tile
 
-    paste_path = os.path.join(tile_dir, best_match)
-    with Image.open(paste_path).convert("RGB") as paste_image:
-        im.paste(paste_image, coordinates)
+    return best_match
 
 
-def compose_mosaic(tile_dir, json_filename, template_file, image_name):
+def rename_mosaic(template_image):
+    """
+    Creates filename for mosaic image
+    """
+
+    if template_image.lower().endswith(('.png', '.jpg', '.jpeg')):
+        image_split = template_image.rsplit(".", 1)
+        mosaic_name = image_split[0]+"_mosaic."+image_split[1]
+    else:
+        mosaic_name = mosaic_name[0]+"_mosaic"
+
+    return mosaic_name
+
+
+def recursive_save(file, image, pathname="", x=1):
+    """
+    Saves copies of files in a 'filename(#).ext' format
+    """
+
+    pathname = file.rsplit("/", 1)[0]
+    if os.path.exists(file):
+        if file.lower().endswith(('.png', '.jpg', '.jpeg')):
+            file_split = file.rsplit(".", 1)
+            ext = file_split[1]
+            file = file_split[0]
+        basename = file.split("(")[0]
+        file = f"{basename}({x}).{ext}"
+        x += 1
+        recursive_save(file, image, x=x)
+    else:
+        image.save(file, "JPEG")
+
+    return file
+
+
+def open_file(filename):
+    if sys.platform == "win32":
+        os.startfile(filename)
+    else:
+        opener = "open" if sys.platform == "darwin" else "xdg-open"
+        subprocess.call([opener, filename])
+
+
+def compose_mosaic(tile_dir, json_filename, template_file, mosaic_name, mosaic_dir="mosaics"):
     """
     Breaks portrait into small square tiles.
     Uses json file to find best suited match for each tile.
@@ -165,18 +178,23 @@ def compose_mosaic(tile_dir, json_filename, template_file, image_name):
                     portrait_rgb, coordinates = crop_mosaic_get_rgb(size, a, b, im)
 
                     # Find the best tile match and paste to mosaic
-                    find_best_match(tile_dir, tile_data, portrait_rgb, im, coordinates)
+                    best_match = find_best_match(tile_data, portrait_rgb)
 
-        im_dir = os.path.join(os.getcwd(), "mosaics")
-        # Check for 'mosaics' directory
-        if not os.path.exists(im_dir):
-            os.mkdir(im_dir)
-        im_path = os.path.join(im_dir, image_name+".jpg")
-        if not im_path:
-            im.save(im_path, "JPEG")
-        with Image.open(template_file).convert("RGB") as original:
-            original.show(template_file)
-        im.show(im_path)
+                    # Paste best match tile onto mosaic
+                    paste_path = os.path.join(tile_dir, best_match)
+                    with Image.open(paste_path).convert("RGB") as paste_tile:
+                        im.paste(paste_tile, coordinates)
+
+        mosaic_file = os.path.join(mosaic_dir, mosaic_name)
+        mosaic_file = recursive_save(mosaic_file, im)
+        basename = mosaic_file.rsplit("/", 1)[1]
+        print(f"Saved {basename}.")
+
+    # Shows both the orginal and mosaic images
+    open_file(template_file)
+    open_file(mosaic_file)
+
+    return
 
 
 def file_cleanup(json_filename, tile_dir=""):
@@ -189,42 +207,31 @@ def file_cleanup(json_filename, tile_dir=""):
         subprocess.run(["rm", "-r", tile_dir])
 
 
-def verify_CLA():
+def verify_args():
     """
     Verifies the command line arguments given by user.
     """
+
     arguments = sys.argv[1:]
-    valid_ext = ["jpg", "jpeg", "png"]
     try:
-        # Argument 1 checks
+        # Verifies image and image_dir exist
         if not os.path.exists(arguments[0]):
             sys.exit(f"Could not find {arguments[0]}.")
+        if not os.path.exists(arguments[1]):
+            sys.exit(f"Could not find {arguments[1]}.")
+
+        # Verify image file has a size and image dir has files
         if os.path.getsize(arguments[0]) == 0:
             sys.exit(f"{arguments[0]} is an invalid file.")
-        if arguments[0].split(".")[-1] not in valid_ext:
-            print(arguments[0].split(".")[-1])
+        if len(os.listdir(arguments[1])) == 0:
+            sys.exit(f"There are no files in {arguments[1]}.")
+
+        # Verify image is an image file
+        if not arguments[0].lower().endswith(('.png', '.jpg', '.jpeg')):
             sys.exit(f"{arguments[0]} is an invalid file type.")
 
-        # Argument 2 checks
-        if len(arguments) > 1:
-            if not os.path.exists(arguments[1]):
-                sys.exit(f"Could not find {arguments[1]}.")
-            if len(os.listdir(arguments[1])) == 0:
-                sys.exit(f"There are no files in {arguments[1]}.")
     except IndexError as e:
         sys.exit(e)
-
-
-def disk_usage():
-    """
-    Checks size of directory/subdirectories after program runs.
-    If over 1 GB, prints a message on the console.
-    """
-
-    file_size = str(subprocess.check_output(["du",  "-s"]))
-    file_size = int(re.findall(r"\d+", file_size)[0])/1000/1000
-    if file_size >= 1:
-        print("This directory is over 1 GB.")
 
 
 def logging(data=""):
@@ -232,6 +239,8 @@ def logging(data=""):
     Creates log.txt if none exists.
     Logs data/time, command line args, and any user input.
     """
+
+    subprocess.run("echo ------------------------------------ >> log.txt", shell=True)
 
     if not os.path.exists("log.txt"):
         subprocess.run(["touch", "log.txt"])
@@ -248,24 +257,20 @@ def logging(data=""):
         string = f"User input: '{data}'"
         subprocess.run(f"echo {string} >> log.txt", shell=True)
 
-    subprocess.run("echo ------------------------------------ >> log.txt", shell=True)
-
-
 
 def main():
-    # Only 2 command line arguments are valid
+    # Logging
+    logging()
+
+    # Verify command line args
     if len(sys.argv) != 3:
         sys.exit("Must have 2 command line arguments <program> <image_name> <image_directory>.")
+    verify_args()
 
-    # verify_CLA()
-
+    # Create variables from args
     template_image = sys.argv[1]
     image_dir = sys.argv[2]
-    mosaic_name = template_image.split(".")
-    try:
-        mosaic_name = mosaic_name[0]+"_mosaic."+mosaic_name[1]
-    except IndexError:
-        pass
+    mosaic_name = rename_mosaic(template_image)
 
     # Derive dimensions from portrait
     with Image.open(template_image).convert("RGB") as im:
@@ -273,18 +278,20 @@ def main():
         size = (size, size)
 
     # Create the tiles
-    tile_dir, json_filename = create_tiles(template_image, image_dir)
+    tile_dir, json_filename = create_tiles(template_image, size, image_dir)
 
-    # Compose the mosaic
-    compose_mosaic(tile_dir, json_filename, template_image, image_name)
+    # Creates 'mosaics' dir if one does not exist
+    mosaic_dir = os.path.join(os.getcwd(), "mosaics")
+    if not os.path.exists(mosaic_dir):
+        os.mkdir(mosaic_dir)
+
+    # Compose the mosaicQ
+    compose_mosaic(tile_dir, json_filename, template_image, mosaic_name)
 
     # Remove files and directories
     file_cleanup(json_filename, tile_dir)
 
-    # Disk usage, logging, and exit
-    disk_usage()
-    if 'user_input' in locals():
-        logging(user_input)
+    # Exit program
     sys.exit()
 
 
